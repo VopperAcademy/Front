a3e69c285398089d7a1e91a163f752f5f2d03b7c
Edwin Davila <edwin.noviembre.0306@gmail.com>
2025-01-12 20:51:37 -0600

    refactor: migrar componentes a TypeScript y mejorar la configuración del proyecto

--- vopper-academy-front/src/app/cursos/page.tsx
+++ vopper-academy-front/src/app/cursos/page.tsx
@@ -1,9 +1,7 @@
 import CourseCard from "../../components/CourseCard";
 
-
 function Cursos() {
-  
-  const image = "/img/courseImg.png"
+  const image = "/img/courseImg.png";
 
   const Cursos = [
     {
@@ -11,154 +9,160 @@ function Cursos() {
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
-    },{
-      image: image,
-      path: "https://via.placeholder.com/150",
-      titulo: "Curso de React",
-      academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
+      tecnologias: ["React", "JavaScript"],
     },
     {
       image: image,
       path: "https://via.placeholder.com/150",
       titulo: "Curso de React",
       academia: "Academia de Código",
-      tecnologias: ["React", "JavaScript"]
-    } 
-  ]
+      tecnologias: ["React", "JavaScript"],
+    },
+    {
+      image: image,
+      path: "https://via.placeholder.com/150",
+      titulo: "Curso de React",
+      academia: "Academia de Código",
+      tecnologias: ["React", "JavaScript"],
+    },
+  ];
 
   return (
-    <section className="grid grid-cols-4 gap-10 overflow-y-auto max-h-[700px] no-scroll-cursor py-10">
-      {
-        Cursos.map((curso, index) => (
-          <CourseCard key={index}
-            img={curso.image}
-            titulo={curso.titulo}
-            academia={curso.academia}
-            tecnologias={curso.tecnologias}
+    <section
+      className="grid grid-cols-4 gap-10 overflow-y-auto max-h-[680px] pb-10 px-5 w-full"
+      style={{
+        scrollbarWidth: "auto",
+        scrollbarColor: "#A9A9A9 #26282D",
+      }}
+    >
+      {Cursos.map((curso, index) => (
+        <CourseCard
+          key={index}
+          img={curso.image}
+          titulo={curso.titulo}
+          academia={curso.academia}
+          tecnologias={curso.tecnologias}
         />
-        ))
-      }
+      ))}
     </section>
   );
 }
