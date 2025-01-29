6d9ca20e6e331ad72585e03b5c5f559932ab60db
SebitasDev <sramireg13@eafit.edu.co>
2025-01-28 20:54:23 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,29 +4,9 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'encrypted-tbn0.gstatic.com', // Dominio del cual proviene la imagen
+                hostname: 'vopper-academy.b-cdn.net', // Dominio del cual proviene la imagen
                 pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com', // Dominio del cual proviene la imagen
-                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
-            },
-            {
-                protocol: 'https',
-                hostname: 'codigofacilito.com', // Dominio del cual proviene la imagen
-                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
-              },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com', // Dominio del cual proviene la imagen
-                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
-            },
-            {
-                protocol: 'https',
-                hostname: 'thumbs.cdn.mdstrm.com', // Dominio del cual proviene la imagen
-                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
-            },
+            }
         ],
     },
     
