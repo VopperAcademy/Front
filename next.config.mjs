1ae49a8958090bece5c7329396e8fb042ebbceca
Edwin Davila <edwin.noviembre.0306@gmail.com>
2025-01-28 23:15:25 -0600

    feat: optimizar configuración de imágenes y mejorar conteo de capítulos en el layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'vopper-academy.b-cdn.net', // Dominio del cual proviene la imagen
-                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
-            }
-        ],
+        unoptimized: true,
     },
     
 };
