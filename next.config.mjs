7a4b117a2f444e7c34d2fbdce4acee0b245244de
Wero09A <barbariverafelipe@gmail.com>
2025-01-29 23:04:21 -0600

    feat: Añadir PopUp en el reproductor de videos para que si o si se tenga que hacer click en el anuncio -Codigo incrustado en cursos/modulo/[id_modulo]/[id_capitulo]/page.tsx, el codigo esta especificado por si alguna vez se quere seaparar

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,21 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'vopper-academy.b-cdn.net', // Dominio del cual proviene la imagen
                 pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
+            },
+            {
+                protocol: 'https',
+                hostname: 'thumbs.cdn.mdstrm.com', // Dominio del cual proviene la imagen
+                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com', // Dominio del cual proviene la imagen
+                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
+            },
+            {
+                protocol: 'https',
+                hostname: 'thumbs.cdn.mdstrm.com', // Dominio del cual proviene la imagen
+                pathname: '/**', // Coincide con la ruta de las imágenes (puedes ajustarlo si la ruta varía)
             }
         ],
     },
